d9ccb29d52ec39dbea4c87d4bd8f9e7144e8ae53
Jayanth Varavani <jnvarava@ncsu.edu>
2011-10-10 17:31:19 -0400

    531

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
  before_filter :authenticate, :only => [:create, :destroy]
  before_filter :admin_user, :only => :destroy
+ before_filter :authorized_user, :only => :destroy
 
  def new
    @title = "microposts"
