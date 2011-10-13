652b9897428fff134324c3b8e71ccfc15f136677
Jayanth Varavani <jnvarava@ncsu.edu>
2011-10-13 01:14:40 -0400

    114

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,29 @@
 class CommentsController < ApplicationController
-     def create
+    def new
+      @title = "comments"
+    end
+
+    def create
 
         @post = Micropost.find_by_id(params[:id])
          #micropost_id = :id
         @comment = @post.comments.build(params[:comment])
          @comment.User_name = params[:User_name]
-		@comment.user_id = params[:User_id]
+         #@comment.user_id = User.find_by_sql("select id from users where name = 'params[:User_name]'")
+          @comment.user_id = params[:User_id]
           @post.increment(:weight)
         if @comment.save && @post.save
-      flash[:success] = "Micropost created!"
-      redirect_to root_path
-    else
-      @feed_items = []
-      render 'pages/home'
+          flash[:success] = "Micropost created!"
+          redirect_to root_path
+        else
+          @feed_items = []
+          render 'pages/home'
+        end
+
     end
 
-     end
-
      def increment
-
+       puts "inside increment"
        @comment = Comment.find(params[:id])
        post = @comment.micropost_id
        @micropost = Micropost.find_by_id(post)
@@ -30,8 +35,7 @@ class CommentsController < ApplicationController
 
      end
 
-	def show
+     def show
        increment
      end
-
   end
